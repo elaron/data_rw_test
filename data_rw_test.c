7adc360040e5a997890749428a52e1c77a598b04
韦钰迪 <elarwei@gmail.com>
2016-12-22 22:03:17 +0800

    success compiled source file

--- data_rw_test.c
+++ data_rw_test.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 
 #define OFFSET_512K 		(19)
+#define OFFSET_1G 		(30)
 #define OFFSET_1T 		(40)
 #define BASE_DATA_BLOCK_SIZE 	(1 << OFFSET_512K)			//512K
 #define ARRAY_LEN		(BASE_DATA_BLOCK_SIZE >> 2)		//512K/4Byte
@@ -18,24 +19,24 @@
 
 typedef enum OP_RESULT
 {
-	OP_ERROR,
-	OP_OK,
-	OP_RESULT_BUTT
-};
+    OP_ERROR,
+    OP_OK,
+    OP_RESULT_BUTT
+}OP_RESULT;
 
 typedef enum DATA_TYPE
 {
     COLD_DATA,
     HOT_DATA,
     DATA_TYPE_BUTT
-};
+}DATA_TYPE;
 
 typedef enum REQUEST_TYPE
 {
     READ_REQ,
     WRITE_REQ,
     REQ_TYPE_BUTT
-};
+}REQUEST_TYPE;
 
 uint32_t g_FileSize;
 uint32_t g_Rate_rw, g_Rate_hot;
@@ -51,7 +52,7 @@ char g_FilePath[PATH_MAX];
 typedef struct BaseDataBlock
 {
     uint32_t data[ARRAY_LEN];
-};
+}BaseDataBlock;
 
 BaseDataBlock g_Template[DATA_TYPE_BUTT];
 
@@ -62,5 +63,232 @@ uint64_t get_curr_time()
 
     return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
+
+void init_rand_seed()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    g_seed[2] = (unsigned short)tv.tv_usec;
+    g_seed[1] = (unsigned short)tv.tv_usec >> 10;
+    g_seed[0] = (unsigned short)tv.tv_sec;
+
+}
+
+uint32_t get_random_num(uint32_t range)
+{
+    if(0 == g_DataDisMode)
+    {
+        init_rand_seed();
+    }
+
+    return (nrand48(g_seed) % range);
+}
+
+void init_data_template()
+{
+    uint32_t i;
+    for(i = 0; i < ARRAY_LEN; i++)
+    {
+        g_Template[COLD_DATA].data[i] = i;
+        g_Template[HOT_DATA].data[i] = i << 1;
+    }
+}
+
+int init_data()
+{
+    uint32_t i;
+    g_DataBlockNum = (g_FileSize >> OFFSET_1G) / sizeof(BaseDataBlock);
+    g_HotBlockNum = g_Rate_hot * g_DataBlockNum / 100;
+
+    init_rand_seed();
+    init_data_template();
+
+    if(g_DataBlockNum > MAX_DATA_BLOCK_NUM)
+    {
+        printf("Illegal DataBlockNum:%d, legal range[1, %d]\n", g_DataBlockNum, MAX_DATA_BLOCK_NUM);
+        return OP_ERROR;
+    }
+
+    int fd = open(g_FilePath, O_RDWR | O_APPEND | O_CREAT, 0666);
+    {
+        if(-1 == fd)
+        {
+            printf("Open file fail! Err:%s Path=%s\n", strerror(errno), g_FilePath);
+            return OP_ERROR;
+        }
+    }
+
+    //init data block
+    memset(g_DataBlockAttr, 0, sizeof(g_DataBlockAttr));
+    if(100 > g_Rate_hot)
+    {
+        int hot_block_num = 0;
+        DATA_TYPE dataType;
+        char c[DATA_TYPE_BUTT];
+
+        c[HOT_DATA] = '1';
+        c[COLD_DATA] = '0';
+
+        for(i = 0; i < g_DataBlockNum; i++)
+        {
+            if(get_random_num(100) <= g_Rate_hot)
+            {
+                dataType = HOT_DATA;
+            }
+            else
+            {
+                dataType = COLD_DATA;
+            }
+
+            g_DataBlockAttr[i] = dataType;
+            
+            if(-1 == write(fd, &g_Template[dataType], sizeof(BaseDataBlock)))
+            {
+                printf("Block %d Write Err:%s\n", i, strerror(errno));
+            }
+
+            printf(" %c", c[dataType]);
+        }
+
+        printf("HotDataRate=%f\n", (double)hot_block_num/(double)g_DataBlockNum);
+    }
+    else
+    {
+        for(i = 0; i < g_DataBlockNum; i++)
+        {
+            g_DataBlockAttr[i] = HOT_DATA;
+            if(-1 == write(fd, &g_Template[HOT_DATA], sizeof(BASE_DATA_BLOCK_SIZE)))
+            {
+                printf("Block %d Write Err:%s\n", i, strerror(errno));
+            }
+        }
+    }
+
+    close(fd);
+    return OP_OK;
+}
+
+int init_param(char** argv)
+{
+    g_FileSize = atoi(argv[1]);
+    g_Rate_rw = atoi(argv[2]);
+    g_Rate_hot = atoi(argv[3]);
+    g_TestTime = atoi(argv[4]);
+    g_DataDisMode = atoi(argv[5]);
+    
+    memset(g_FilePath, 0, sizeof(g_FilePath));
+    strncpy(g_FilePath, argv[6], sizeof(g_FilePath));
+
+    if(g_Rate_rw > 100 || g_Rate_hot > 100)
+    {
+        printf("Illegal rate Rate_rw:%d Rate_hot:%d, legal range[, 100]\n", g_Rate_rw, g_Rate_hot);
+        return OP_ERROR;
+    }
+    
+    if(g_FileSize > 1024)
+    {
+        printf("File too large(%dG), legal range is [1, 1024]G\n", g_FileSize);
+        return OP_ERROR;
+    }
+
+    if(g_TestTime == 0)
+    {
+        printf("Illegal TestTime, must larger than 0\n");
+        return OP_ERROR;
+    }
+
+    printf("FileSize:%d \nRate_rw:%d \nRate_hot:%d \nTestTime:%d \nDataDisMode:%d \nFilePath:%s\n",
+           g_FileSize,
+           g_Rate_rw,
+           g_Rate_hot,
+           g_TestTime,
+           g_DataDisMode,
+           g_FilePath);
+
+    return OP_OK;
+}
+
+REQUEST_TYPE get_request_type()
+{
+    return WRITE_REQ;
+}
+
+uint32_t get_data_block_index()
+{
+    DATA_TYPE dataType = (get_random_num(100) >= g_Rate_hot) ? HOT_DATA : COLD_DATA;
+    uint32_t blkIdx = get_random_num(g_DataBlockNum);
+    uint32_t i;
+
+    if(g_DataBlockAttr[blkIdx % MAX_DATA_BLOCK_NUM] == dataType)
+    {
+        return blkIdx;
+    }
+
+    for(i = blkIdx + 1; i < g_DataBlockNum; i++)
+    {
+        if(g_DataBlockAttr[blkIdx % MAX_DATA_BLOCK_NUM] == dataType)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+void write_data()
+{
+    uint32_t blkIdx = get_data_block_index();
+}
+
+int main(int argc, char** argv)
+{
+    REQUEST_TYPE request;
+    uint64_t start;
+
+    if(7 != argc)
+    {
+        printf("Useage: data_rw_test FileSize(G) Rate_rw Rate_hot TestTime(s) DataDisMode FilePath\n");
+        return 0;
+    }
+
+    if(OP_ERROR == init_param(argv))
+    {
+        return 0;
+    }
+
+    if(OP_ERROR == init_data())
+    {
+        return 0;
+    }
+
+    start = get_curr_time();
+
+    do
+    {
+        request = get_request_type();
+        switch(request)
+        {
+            case WRITE_REQ:
+                write_data();
+                break;
+
+            default:
+                printf("Illegal request_type\n");
+       }
+
+       if(get_curr_time() - start > (g_TestTime << 10))
+       {
+           break;
+       }
+    }while(1);
+
+    return 0;
+}
+
+
+
+
+
 
 
