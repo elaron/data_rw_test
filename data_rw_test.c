d5efe3503c5e48bb3c2d876c91ee752b02c62845
root <root@localhost.localdomain>
2017-01-21 03:54:34 +0800

    refine code

--- data_rw_test.c
+++ data_rw_test.c
@@ -35,25 +35,47 @@ typedef enum REQUEST_TYPE
 {
     READ_REQ,
     WRITE_REQ,
-    REQ_TYPE_BUTT
+    REQUEST_TYPE_BUTT
 }REQUEST_TYPE;
 
 uint32_t g_FileSize;
 uint32_t g_Rate_rw, g_Rate_hot;
-uint32_t g_TestTime;
-uint32_t g_DataBlockNum;
+uint64_t g_TestTime;
+uint64_t g_TestStartTime;
+uint64_t g_DataBlockNum;
+uint32_t g_BlockSize;	//k
+uint32_t g_BlockSize_Byte;
 
 unsigned short g_seed[3];
 uint8_t g_DataBlockAttr[MAX_DATA_BLOCK_NUM];
 uint8_t g_DataDisMode;                       //data distribution mode
 char g_FilePath[PATH_MAX];
 
+int g_Fd;
+
+typedef struct Stat
+{
+    uint32_t proc_blk_cnt[REQUEST_TYPE_BUTT];
+}Stat;
+
+Stat g_Stat;
+
+typedef enum THREAD_TYPE
+{
+    MONITOR_THRD,
+    DO_TEST_THRD,
+    THREAD_TYPE_BUTT
+}THREAD_TYPE;
+
+pthread_t g_Thread[THREAD_TYPE_BUTT];
+
 typedef struct BaseDataBlock
 {
-    uint32_t data[ARRAY_LEN];
+    uint32_t arr[ARRAY_LEN];
 }BaseDataBlock;
 
-BaseDataBlock g_Template[DATA_TYPE_BUTT];
+char* g_pTemplate[DATA_TYPE_BUTT];
+char* g_pBuff; //for data read
 
 uint64_t get_curr_time()
 {
@@ -68,14 +90,20 @@ void init_rand_seed()
     struct timeval tv;
     gettimeofday(&tv, NULL);
 
-    g_seed[2] = (unsigned short)tv.tv_usec;
+    g_seed[0] = (unsigned short)tv.tv_usec;
     g_seed[1] = (unsigned short)tv.tv_usec >> 10;
-    g_seed[0] = (unsigned short)tv.tv_sec;
+    g_seed[2] = (unsigned short)tv.tv_sec;
 
 }
 
 uint32_t get_random_num(uint32_t range)
 {
+    if(0 == range)
+    {
+        printf("ERR: range == 0\n");
+        return 0;
+    }
+
     if(0 == g_DataDisMode)
     {
         init_rand_seed();
@@ -84,75 +112,60 @@ uint32_t get_random_num(uint32_t range)
     return (nrand48(g_seed) % range);
 }
 
-void init_data_template()
+int init_data_template()
 {
-    uint32_t i;
-    for(i = 0; i < ARRAY_LEN; i++)
-    {
-        g_Template[COLD_DATA].data[i] = i;
-        g_Template[HOT_DATA].data[i] = i << 1;
-    }
+   uint32_t bcnt, i;
+  if(NULL == (g_pBuff = (char*)calloc(g_BlockSize, sizeof(BaseDataBlock)))) 
+  {
+      printf("Alloc read buffer fail!\n");
+      return OP_ERROR;
+  }
+
+  DATA_TYPE dataType;
+  for(dataType = 0; dataType < DATA_TYPE_BUTT; dataType++)
+  {
+      g_pTemplate[dataType] = (char*)calloc(g_BlockSize, sizeof(BaseDataBlock));
+      if(NULL == g_pTemplate[dataType])
+      {
+          printf("Alloc write template fail! dataType=%d\n", dataType);
+          return OP_ERROR;
+      }
+  }
+
+  //init data content
+  BaseDataBlock *pHotBlock, *pColdBlock;
+  pHotBlock = (BaseDataBlock*)g_pTemplate[HOT_DATA];
+  pColdBlock = (BaseDataBlock*)g_pTemplate[COLD_DATA];
+
+  for(bcnt = 0; bcnt < g_BlockSize; bcnt++)
+  {
+      for(i = 0; i < ARRAY_LEN; i++)
+      {
+          pHotBlock->arr[i] = i;
+          pColdBlock->arr[i] = (i << 1);
+      }
+      pHotBlock++;
+      pColdBlock++;
+  }
+
+  return OP_OK;
 }
 
-void init_data_block(int fd, DATA_TYPE dataType, uint32_t blkIdx)
+void init_data_block(DATA_TYPE dataType, uint32_t blkIdx)
 {
     g_DataBlockAttr[blkIdx] = dataType;
-
-    if(-1 == write(fd, &g_Template[dataType], sizeof(BaseDataBlock)))
+#ifdef INIT_FILE
+    if(-1 == write(g_Fd, g_pTemplate[dataType], sizeof(BaseDataBlock)))
     {
         printf("Block %d Write Err:%s\n", blkIdx, strerror(errno));
     }
-
+#endif
     printf(" %d", dataType);
-}
-
-int init_file()
-{
-    uint32_t i;
-    DATA_TYPE dataType;
-    int block_num[DATA_TYPE_BUTT] = {0};
-
-    int fd = open(g_FilePath, O_RDWR | O_APPEND | O_CREAT, 0666);
-    {
-        if(-1 == fd)
-        {
-            printf("Open file fail! Err:%s Path=%s\n", strerror(errno), g_FilePath);
-            return OP_ERROR;
-        }
-    }
-
-    //init data block
-    memset(g_DataBlockAttr, 0, sizeof(g_DataBlockAttr));
-
-    if(100 > g_Rate_hot)
-    {
-        for(i = 0; i < g_DataBlockNum; i++)
-        {
-            dataType = (get_random_num(100) <= g_Rate_hot) ? HOT_DATA : COLD_DATA;
-            init_data_block(fd, dataType, i);
-            block_num[dataType] += 1;
-        }
-    }
-    else
-    {
-        for(i = 0; i < g_DataBlockNum; i++)
-        {
-            dataType = HOT_DATA;
-            init_data_block(fd, dataType, i);
-        }
-        block_num[HOT_DATA] = g_DataBlockNum;
-        block_num[COLD_DATA] = 0;
-    }
-
-    printf("HotDataRate=%f\n", (double)block_num[HOT_DATA]/(double)g_DataBlockNum);
-    close(fd);
-
-    return OP_OK;
 }
 
 int init_data()
 {
-    g_DataBlockNum = (g_FileSize >> OFFSET_1G) / sizeof(BaseDataBlock);
+    g_DataBlockNum = ((uint64_t)g_FileSize<<30)/(g_BlockSize * sizeof(BaseDataBlock)); 
 
     if(g_DataBlockNum > MAX_DATA_BLOCK_NUM)
     {
@@ -161,9 +174,57 @@ int init_data()
     }
 
     init_rand_seed();
-    init_data_template();
+    if(OP_ERROR == init_data_template())
+    {
+        return OP_ERROR;
+    }
+
+    if(0 != posix_memalign((void**)&g_pTemplate, 1024, g_BlockSize_Byte * DATA_TYPE_BUTT))
+    {
+        printf("Posix_memalign failed!");
+        return OP_ERROR;
+    }
+
+    g_Fd = open(g_FilePath, O_RDWR | O_CREAT | O_SYNC, 0666);
+    if(-1 == g_Fd)
+    {
+        printf("Open file fail! Err = %s, filePath = %s\n", strerror(errno), g_FilePath);
+        return OP_ERROR;
+    }
     
-    return init_file();
+    //init data block
+    uint64_t i;
+    int hot_block_num = 0;
+    DATA_TYPE dataType;
+    int block_num[DATA_TYPE_BUTT] = {0};
+
+    memset(g_DataBlockAttr, 0, sizeof(g_DataBlockAttr));
+    if(100 == g_Rate_hot)
+    {
+        //pure hot data
+        for(i = 0; i < g_DataBlockNum; i++)
+        {
+            dataType = HOT_DATA;
+            init_data_block(dataType, i);
+        }
+        block_num[HOT_DATA] = g_DataBlockNum;
+    }
+    else
+    {
+        //cold hot data mixed
+        for(i = 0; i < g_DataBlockNum; i++)
+        {
+            dataType = (get_random_num(100) <= g_Rate_hot) ? HOT_DATA : COLD_DATA;
+            init_data_block(dataType, i);
+            block_num[dataType] += 1;
+        }
+    }
+
+    printf("\n Set data block attribute finish!\n blockCount:%ld hotBlock:%ld hotDataRate:%f\n",
+                    g_DataBlockNum,
+                    block_num[HOT_DATA],
+                    (double)block_num[HOT_DATA] / (double)g_DataBlockNum);
+    return OP_OK;
 }
 
 int init_param(char** argv)
